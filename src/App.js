d9a9316d327b7a911503ae32d655482abf7a796a
Delia Paternina <delia.paternina@gmail.com>
2025-02-28 16:28:57 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Box,
+  Button,
+  Heading,
+  SimpleGrid,
+  Text,
+  Image,
+  Stack,
+} from "@chakra-ui/react";
+
+const products = [
+  { id: 1, name: "Laptop", price: "$999", image: "/images/Laptop.jpg" },
+  {
+    id: 2,
+    name: "Smartphone",
+    price: "$699",
+    image: "/images/Smartphone.webp",
+  },
+  { id: 3, name: "Headphones", price: "$199", image: "/images/Headphones.jpg" },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box p={5}>
+      <Heading mb={6} textAlign="center">
+        Product List
+      </Heading>
+      <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={6}>
+        {products.map((product) => (
+          <Box
+            key={product.id}
+            p={5}
+            borderWidth="1px"
+            borderRadius="lg"
+            boxShadow="md"
+          >
+            <Image src={product.image} alt={product.name} borderRadius="md" />
+            <Stack spacing={3} mt={3}>
+              <Text fontWeight="bold" fontSize="xl">
+                {product.name}
+              </Text>
+              <Text color="gray.500">{product.price}</Text>
+              <Button colorScheme="blue" variand="solid">
+                Buy Now
+              </Button>
+            </Stack>
+          </Box>
+        ))}
+      </SimpleGrid>
+    </Box>
   );
 }
 
